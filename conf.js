1009a4fb9d2741213341b1cfbc1e3dbce9b67bf0
magb7 <margaux.bedu@gmail.com>
2020-06-18 10:45:05 +0200

    create file conf.js

---
+++ conf.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+require("dotenv").config();
+const { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
+module.exports = connection;
