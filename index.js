de69fdcf7b03609346a801d065ae95ead42df456
magb7 <margaux.bedu@gmail.com>
2020-07-07 18:41:54 +0200

    add cors package

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5000;
 const cities = require("./routes/cities");
 
 // Config
 app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
