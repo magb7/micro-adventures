534736190aae2dfd9664a57a4d2d949e07f5c21b
magb7 <margaux.bedu@gmail.com>
2020-07-07 16:38:21 +0200

    import mysql2

--- conf.js
+++ conf.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
-require("dotenv").config();
+const mysql = require("mysql2/promise");
 const { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
 });
-module.exports = connection;
+
+module.exports = { connection };
