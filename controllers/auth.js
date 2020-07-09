177f413a8aa761b4e655b5291d9768f3563dee83
magb7 <margaux.bedu@gmail.com>
2020-07-09 16:46:37 +0200

    implement controller for signup routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,15 @@
 const { connection } = require("../conf");
 
-const createUser = (req, res) => {
-  return res.status(200).send("I am in POST signup");
+const createUser = async (req, res) => {
+  try {
+    const user = await connection.query("INSERT INTO user SET ?", [req.body]);
+    return res.status(200).send(req.body);
+  } catch (err) {
+    res.status(500).send("Error while creating user");
+    console.log(err.sql);
+    console.log(err.message);
+    return;
+  }
 };
 
 module.exports = { createUser };
