4a27fade2fd2cdd9eb0aebbe49922d6966a6b5f4
magb7 <margaux.bedu@gmail.com>
2020-07-10 12:34:05 +0200

    sucessefully add password hash and jwt in createUser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,27 @@
 const { connection } = require("../conf");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const createUser = async (req, res) => {
-  try {
-    const user = await connection.query("INSERT INTO user SET ?", [req.body]);
-    return res.status(200).send(req.body);
-  } catch (err) {
-    res.status(500).send("Error while creating user");
-    console.log(err.sql);
-    console.log(err.message);
-    return;
-  }
+const createUser = (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, 8);
+  connection.query("INSERT INTO user SET ?", [req.body], (errReq, resReq) => {
+    if (errReq) {
+      console.log(errReq.sql);
+      console.log(errReq.message);
+      return res.status(500).send("Error while creating user");
+    }
+    const user = {
+      mail: req.body.email,
+      id: resReq.insertId,
+    };
+
+    const token = jwt.sign(user, "secret");
+
+    return res.status(200).send({
+      user,
+      token,
+    });
+  });
 };
 
 module.exports = { createUser };
