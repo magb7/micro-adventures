41f1c316bee67837051aa9653d86e24669415faa
magb7 <margaux.bedu@gmail.com>
2020-07-10 14:46:01 +0200

    import variable for the token secret

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,29 @@
-const { connection } = require("../conf");
+const { connection, tokenSecret } = require("../conf");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const createUser = (req, res) => {
+  // Password encryption
   req.body.password = bcrypt.hashSync(req.body.password, 8);
+
+  // Insertion in database
   connection.query("INSERT INTO user SET ?", [req.body], (errReq, resReq) => {
     if (errReq) {
       console.log(errReq.sql);
       console.log(errReq.message);
       return res.status(500).send("Error while creating user");
     }
+
+    // Creation of a user to make token
     const user = {
       mail: req.body.email,
       id: resReq.insertId,
     };
 
-    const token = jwt.sign(user, "secret");
+    // Creation of the token
+    const token = jwt.sign(user, `${tokenSecret}`);
 
+    // Sending back to the user the token and minot information
     return res.status(200).send({
       user,
       token,
@@ -24,4 +31,6 @@ const createUser = (req, res) => {
   });
 };
 
-module.exports = { createUser };
+const connectUser = (req, res) => {};
+
+module.exports = { createUser, connectUser };
